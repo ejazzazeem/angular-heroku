993c5753e8daf26d66a9728d753f01d99507759d
ejazzazeem <ejazzazeem@gmail.com>
2020-04-28 20:07:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
- const express = require('express');
-const path = require('path');
-const ngApp = express();
-ngApp.use(express.static('./dist/'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/index.html'));
-});
-ngApp.listen(process.env.PORT || 8080);
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/angular-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
+
+app.listen(process.env.PORT || 8080);
